bb0f9b715f29bb1bcba6a7c45f9ddd9859b643e0
bcaliva21 <bcaliva21@gmail.com>
2021-11-11 16:28:25 -0800

    add some comments and create the circleCompare fan to compare settings to the fanGroup fan.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { FlakesTexture } from 'three/examples/jsm/textures/FlakesTexture.js'
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
 import * as dat from 'dat.gui'
-import { Group, Vector2 } from 'three'
+import { Group, Vector2, WebGLCubeRenderTarget } from 'three'
 
 // controls in top right corner of page
 // could be cool to implement later to change effects on the fan
@@ -16,12 +16,16 @@ const scene = new THREE.Scene()
 
 // textures
 const textureLoader = new THREE.TextureLoader()
-const designPattern = textureLoader.load('./static/designs/wave.jpeg');
+let designPattern = textureLoader.load('./static/designs/wave.jpeg')
+let designPattern2 = textureLoader.load('./static/designs/mountain_and_boat.jpeg')
+let designPattern3 = textureLoader.load('./static/designs/wave2.jpg')
 const fanTexture = textureLoader.load('./static/textures/fanTexture.jpeg')
 const grainyTexture = textureLoader.load('./static/textures/oldTexture.png')
 const brassTexture = textureLoader.load('./static/textures/brass_albedo.png')
 const brassRoughness = textureLoader.load('./static/textures/brass_roughness.png')
+const dojo = textureLoader.load('./static/backgrounds/dojo.jpeg')
 
+scene.background = dojo;
 
 // fan config
 const fanGeometry = new THREE.CircleGeometry( 1, 30, 0, 2 )
@@ -55,15 +59,35 @@ const wireMaterial = new THREE.LineBasicMaterial({
 const line = new THREE.LineSegments( wireframe, wireMaterial )
 line.side = THREE.DoubleSide
 // setting the textures onto the circle geometry
+
 const fanDesign = new THREE.MeshStandardMaterial({
   map: designPattern,
-  emissiveMap: grainyTexture,
-  emissive: '#664229',
+  normalMap: grainyTexture,
+  normalScale: new THREE.Vector2(0.15, 0.15),
   side: THREE.DoubleSide
   // wireframe: true // cool effect with this on
 })
-const circle = new THREE.Mesh(fanGeometry, fanDesign)
 
+const fanDesignCompare = new THREE.MeshStandardMaterial({
+  map: designPattern,
+  side: THREE.DoubleSide
+  // wireframe: true // cool effect with this on
+})
+
+// // fan specs        width / height
+// const planeAspect = 650 / 500;
+// // image specs      width / height
+// const imageAspect = 192 / 241;
+// const aspect = imageAspect / planeAspect;
+
+// designPattern3.offset.x = aspect > 1 ? (1 - 1 / aspect) / 2 : 0;
+// designPattern3.repeat.x = aspect > 1 ? 1 / aspect : 1;
+
+// designPattern3.offset.y = aspect > 1 ? 0 : (1 - aspect) / 2;
+// designPattern3.repeat.y = aspect > 1 ? 1 : aspect;
+
+const circle = new THREE.Mesh(fanGeometry, fanDesign)
+const circleCompare = new THREE.Mesh(fanGeometry, fanDesignCompare)
 
 // setup handle realistic texture
 let handleTexture = new THREE.CanvasTexture(new FlakesTexture())
@@ -73,8 +97,6 @@ handleTexture.wrapT = THREE.RepeatWrapping
 handleTexture.repeat.x = 10
 handleTexture.repeat.y = 6
 
-
-
 // fan handle
 
 const handleGeometry = new THREE.BoxGeometry( .1, 0.1, 1.05 );
@@ -82,10 +104,11 @@ const handleMats = new THREE.MeshPhysicalMaterial({
   map: brassTexture,
   normalMap: brassRoughness,
   clearcoat: 1,
-  metalness: 0.9,
+  metalness: 0.5,
   roughness: 0.5,
   normalScale: new THREE.Vector2(0.15, 0.15),
   clearcoatRoughness: 0.1,
+  emissiveIntensity: 0.5
 })
 const handleMesh = new THREE.Mesh( handleGeometry, handleMats )
 
@@ -95,6 +118,7 @@ fanGroup.add( line )
 fanGroup.add( handleMesh )
 
 // add the fan to the scene
+// add circleCompare to test textures/designs against the fanGroup
 scene.add( fanGroup )
 console.log(handleMesh)
 
@@ -102,6 +126,7 @@ console.log(handleMesh)
 circle.position.set(-0.8, -0.5, 1)
 line.position.set(-0.8, -0.5, 1)
 handleMesh.position.set(-0.29, -0.55, 1.03)
+circleCompare.position.set(0.8, -0.5, 1)
 
 handleMesh.rotation.y += 1.59
 // view size config
@@ -115,12 +140,13 @@ const color = 0xFFFFFF;
 const intensity = 1;
 const directLightIntensity = 0.1;
 const light = new THREE.AmbientLight(color, intensity);
-const spotLight = new THREE.DirectionalLight('white', directLightIntensity, 50);
+const spotLight = new THREE.DirectionalLight('white', directLightIntensity);
 spotLight.position.set(0, -1.5, 5);
 
 const lightHelper = new THREE.DirectionalLightHelper( spotLight )
 
-scene.add(light, spotLight, spotLight.target, lightHelper);
+scene.add(light, spotLight, lightHelper);
+console.log('fan group: ', fanGroup)
 
 // camera config
 const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height)
