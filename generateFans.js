7823ef87b1120c95fdea27afc240d4fa322d783c
aj <antoniojcaporicci@gmail.com>
2021-11-20 09:23:00 -0600

    add recursive script for dist folder push to s3

--- generateFans.js
+++ generateFans.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const fs = require('fs');
 const AWS = require('aws-sdk');
 
@@ -7,21 +8,50 @@ const s3 = new AWS.S3({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
-const fileName = 'dist';
+const directoryToUpload = 'dist';
+const bucketName = 'fan-nft';
 
-const uploadFile = () => {
-  fs.readFile(fileName, (err, data) => {
-     if (err) throw err;
-     const params = {
-         Bucket: 'fan-nft', // pass your bucket name
-         Key: 'fan', // file will be saved as testBucket/fan
-         Body: JSON.stringify(data, null, 2)
-     };
-     s3.upload(params, function(s3Err, data) {
-         if (s3Err) throw s3Err
-         console.log(`File uploaded successfully at ${data.Location}`)
-     });
+const filePaths = [];
+const getFilePaths = (dir) => {
+  fs.readdirSync(dir).forEach(function (name) {
+    const filePath = path.join(dir, name);
+    const stat = fs.statSync(filePath);
+    if (stat.isFile()) {
+      filePaths.push(filePath);
+    } else if (stat.isDirectory()) {
+      getFilePaths(filePath);
+    }
   });
 };
 
-uploadFile();
+getFilePaths(directoryToUpload);
+
+const uploadToS3 = (dir, path) => {
+  return new Promise((resolve, reject) => {
+    const key = path.split(`${dir}/`)[1];
+    const params = {
+      Bucket: bucketName,
+      Key: key,
+      Body: fs.readFileSync(path),
+    };
+    s3.putObject(params, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log(`uploaded ${params.Key} to ${params.Bucket}`);
+        resolve(path);
+      }
+    });
+  });
+};
+
+const uploadPromises = filePaths.map((path) =>
+  uploadToS3(directoryToUpload, path)
+);
+
+Promise.all(uploadPromises)
+  .then((result) => {
+    console.log('uploads complete');
+    console.log(result);
+  })
+  .catch((err) => console.error(err));
