1e6b420c460ac328cf633593c38f68acb13a560f
bcaliva21 <bcaliva21@gmail.com>
2022-04-03 12:05:36 -0700

    modify gifHelpers to use file-saver left a todo so we know where to proceed next

--- src/utils/gifHelpers.js
+++ src/utils/gifHelpers.js
@@ -20,3 +20,53 @@
 //     CanvasCapture.recordFrame()
 //   }
 // }
+
+import FileSaver from 'file-saver'
+
+// TODO:
+// we will need to step the html like we have it set up in ticket
+// this will make downloding mp4s way more consistent and less of a headache
+
+// to ensure that we don't get multiple downloads
+const once = (fn, context) => {
+	var result;
+
+	return function() {
+		if (fn) {
+			result = fn.apply(context || this, arguments);
+			fn = null;
+		}
+
+		return result;
+	};
+}
+
+export const generateFanGif = (title) => {
+    var canvas = document.querySelector("canvas")
+    var ctx = canvas.getContext("experimental-webgl")
+
+    var video = document.querySelector("video")
+
+    var videoStream = canvas.captureStream(60)
+    var mediaRecorder = new MediaRecorder(videoStream)
+
+    var chunks = []
+    mediaRecorder.ondataavailable = function (e) {
+        chunks.push(e.data)
+    };
+
+    mediaRecorder.onstop = function (e) {
+        var blob = new Blob(chunks, { 'type' : 'video/mp4' })
+        chunks = []
+        var videoURL = URL.createObjectURL(blob)
+        video.src = videoURL
+        setTimeout(() => {
+            // we will need to get the title here
+            once(FileSaver.saveAs(videoURL, `test.mp4`))
+        }, 8000)
+    };
+
+    setTimeout(() => { mediaRecorder.start() }, 500)
+    setTimeout(function () { mediaRecorder.stop() }, 5000)
+
+}
